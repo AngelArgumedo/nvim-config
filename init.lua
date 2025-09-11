fe737d2b250dd1db1ab6005844cc0d05f91a6c73
AngelArgumedo <angel.david.argumedo.guerra@gmail.com>
2025-09-11 17:36:00 -0500

    files

---
+++ init.lua
@@ -0,0 +1,421 @@
+-- =========================
+--   Neovim Config (init.lua)
+-- =========================
+
+-- Bootstrap lazy.nvim (gestor de plugins)
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- última versión estable
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- =========================
+-- Plugins
+-- =========================
+require("lazy").setup({
+  -- Tema
+  { "folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {} },
+
+  -- Dashboard con waifu
+  {
+    "goolord/alpha-nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      local alpha = require("alpha")
+      local dashboard = require("alpha.themes.dashboard")
+
+      --  ASCII Art
+      local headers = {
+          -- Waifu 1
+        {
+          [[ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣷⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠀⠀⠀⠀]],
+          [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣷⣤⣀⠀⠀⠀⠀⠀⠉⠑⣶⣤⣄⣀⣠⣤⣶⣶⣿⣿⣿⣿⡇⠀⠀⠀]],
+          [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⡿⠟⠋⠁⠀⠀⠀⣀⠤⠒⠉⠈⢉⡉⠻⢿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀]],
+          [[⠀⠀⠀⠀⣀⣴⣶⣿⣷⡄⠀⠀⠀⠀⢹⣿⣿⣿⣿⠏⠁⠀⢀⠄⠀⠀⠈⢀⠄⠀⢀⡖⠁⠀⢀⠀⠈⠻⣿⣿⣿⣿⡏⠀⠀⠀⠀]],
+          [[⠀⠀⢠⣾⣿⣿⣿⣿⡿⠁⠀⠀⠀⠀⢸⣿⣿⠏⠀⠀⢀⡴⠁⠀⠀⣠⠖⠁⢀⠞⠋⠀⢠⡇⢸⡄⠀⠀⠈⢻⣿⣿⠁⠀⠀⠀⠀]],
+          [[⠀⣠⣿⣿⣿⣿⣿⠟⠁⠀⠀⠀⠀⠀⢸⡿⠁⠀⠀⢀⡞⠀⠀⢀⡴⠃⠀⣰⠋⠀⠀⣰⡿⠀⡜⢳⡀⠘⣦⠀⢿⡇⠀⠀⠀⠀⠀]],
+          [[⢠⣿⣿⣿⣿⡿⠃⠀⠀⠀⠀⠀⠀⢰⣿⠃⠀⢀⠆⡞⡄⠀⣠⡞⠁⣀⢾⠃⠀⣀⡜⢱⠇⣰⠁⠈⣷⠂⢸⡇⠸⣵⠀⠀⠀⠀⠀]],
+          [[⣿⣿⣿⣿⡿⠁⠀⠀⠀⠀⠀⠀⢠⣿⠇⠀⠀⡜⣸⡟⢀⣴⡏⢠⣾⠋⡎⢀⣼⠋⢀⡎⡰⠃⠀⠀⣿⣓⢒⡇⠀⣿⠀⠀⠀⠀⠀]],
+          [[⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠴⢻⣟⢀⣀⢀⣧⡇⢨⠟⢾⣔⡿⠃⢸⢀⠞⠃⢀⣾⡜⠁⠀⠀⠀⡏⠁⢠⠃⠀⢹⠀⠀⠀⠀⠀]],
+          [[⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⢸⣼⢸⣿⡟⢻⣿⠿⣶⣿⣿⣿⣶⣾⣏⣀⣠⣾⣿⠔⠒⠉⠉⢠⠁⡆⡸⠀⡈⣸⠀⠀⠀⠀⠀]],
+          [[⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⣸⣿⣸⣿⣇⢸⠃⡄⢻⠃⣾⣿⢋⠘⣿⣿⠏⣿⡟⣛⡛⢻⣿⢿⣶⣷⣿⣶⢃⣿⠀⠀⠀⠀⠀]],
+          [[⢸⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⣰⠃⣿⣿⣿⣿⠀⣸⣧⠈⣸⣿⠃⠘⠃⢹⣿⠀⣿⠃⠛⠛⣿⡇⢸⣿⡇⢸⣿⡿⣿⡀⠀⠀⠀⠀]],
+          [[⠀⠻⣿⣿⣿⣿⣦⡀⠀⢀⡔⣹⣼⡟⡟⣿⣿⣿⠛⠻⠶⠿⠷⣾⣿⣿⣬⣿⣠⣿⣀⣿⣿⣿⡇⠸⡿⠀⣾⡏⢠⣿⣇⠀⠀⠀⠀]],
+          [[⠀⠀⠙⢿⣿⣿⣿⣿⣷⡞⢠⣿⢿⡇⣿⡹⡝⢿⡷⣄⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠙⠛⠛⠻⠿⣶⣶⣾⣿⣇⣾⠉⢯⠃⠀⠀⠀]],
+          [[⠀⠀⠀⠀⠙⠿⣿⣿⣿⠇⢸⠇⠘⣇⠸⡇⣿⣮⣳⡀⠉⠂⠀⠀⣀⣤⡤⢤⣀⠀⠀⠀⠀⠀⢈⣿⠟⣠⣾⠿⣿⡆⡄⣧⡀⠀⠀]],
+          [[⠀⠀⠀⠀⠀⠀⠀⠙⠻⡘⠾⣄⠀⠘⢦⣿⠃⠹⣿⣿⣶⠤⠀⠀⣿⠋⠉⠻⣿⠁⠀⠠⣀⣤⣾⣵⣾⡿⠃⣾⠏⣿⣧⠋⡇⠀⠀]],
+          [[⠀⠀⠀⠀⠀⠀⠀⣠⠖⠳⣄⡈⠃⠀⠼⠋⠙⢷⣞⢻⣿⣿⣀⡀⠈⠤⣀⠬⠟⠀⢀⣠⣶⠿⢛⡽⠋⣠⣾⣏⣠⡿⣃⣞⠀⠀⠀]],
+          [[⠀⠀⠀⠀⠀⠀⠀⣧⠀⠀⠀⠉⠛⠓⠢⠶⣶⡤⠺⡟⢺⣿⠿⣿⣶⣤⣀⣠⣴⣾⡿⠿⢵⠋⠙⠲⣏⡝⠁⠀⣹⢿⡣⣌⠒⠄⠀]],
+          [[⠀⠀⠀⠀⠀⠀⢸⠈⡄⠀⠇⠀⠀⡖⠁⢢⡞⠀⢰⠻⣆⡏⣇⠙⠻⣿⣿⣿⣿⠋⢀⡴⣪⢷⡀⠀⡘⠀⢀⠜⠁⢀⠟⢆⠑⢄⠀]],
+          [[⠀⠀⠀⠀⠀⠀⠘⡄⠱⠀⠸⡀⠄⠳⡀⠀⢳⡀⢰⠀⢸⢇⡟⠑⠦⢈⡉⠁⢼⢠⡏⣴⠟⢙⠇⠀⡇⢠⠃⢀⡴⠁⠀⠘⠀⠈⡆]],
+          [[⠀⠀⠀⠀⠀⠀⠀⠇⠀⠣⠀⡗⢣⡀⠘⢄⠀⢧⠀⢳⡟⠛⠙⣧⣧⣠⣄⣀⣠⢿⣶⠁⠀⠸⡀⠀⠓⠚⢴⣋⣠⠔⠀⠀⠀⠀⠁]],
+          [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠧⡤⠙⢤⡈⣦⡼⠀⠀⠧⢶⠚⡇⠈⠁⠈⠃⠀⡰⢿⣄⠀⠀⠑⢤⣀⠀⠀⠀⠈⠁⠀⠀⠀⠀⠀]],
+        },
+
+          -- Waifu 2
+        {
+          [[  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠎⠀⠀⡞⡷⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢧⠘⡍⢳⡀⠀⠀]],
+          [[  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡞⠀⠀⢠⡇⡇⠹⡄⠀⣀⡠⠤⠴⠒⠒⠒⠒⠒⠒⠒⠢⠤⠤⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡔⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢇⠹⡄⢧⠀⠀]],
+          [[  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡜⠀⠀⠀⠈⣷⡇⠀⣻⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠒⠤⢄⡀⠀⠀⠀⣼⢡⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡀⠱⡈⡇⠀]],
+          [[  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠅⠀⠀⢀⣴⣿⡇⢠⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⠢⣼⢹⣼⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⢳⡸⡄]],
+          [[  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠇⠄⠀⡰⠋⠘⢿⣿⡾⢀⡤⠒⠚⠉⠉⠉⠉⠑⠒⠒⠒⠤⠤⠤⠀⣀⡀⠀⠀⠀⠀⠀⠀⠀⡇⡏⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⢇⢳]],
+          [[  ⠀⠀⠘⡄⠀⠀⠀⠀⠀⠀⡼⠸⠀⡼⠁⣠⠔⠳⡉⠀⡜⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠒⠦⣄⡀⠀⠀⡇⣧⡟⣹⠦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠸⡈]],
+          [[  ⠀⠀⠀⠸⡀⠀⠀⠀⠀⢠⠇⡄⣜⡠⠊⠁⠀⠀⠑⠞⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⢳⠀⢻⡿⢡⡟⠀⠙⢄⠀⠀⠀⠀⠀⠀⠀⠠⡀⢹⠀⠀⢧]],
+          [[  ⠀⠀⠀⠀⢱⠀⠀⠀⠀⡸⢀⠛⠋⠀⠀⠀⠀⠀⠀⢀⣾⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢇⠀⢀⢤⣀⠀⠀⠈⢳⡀⠀⠀⠀⠀⠀⠀⠃⢸⠀⠀⠘]],
+          [[  ⠀⠀⠀⠀⠀⢣⢀⡠⠖⡇⠸⠀⠀⠀⡀⠀⠀⠀⢀⢞⣼⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠊⠁⠀⠈⠳⢄⠀⠀⢱⡀⠀⠀⠀⠀⠀⠈⢸⠀⠀⠀]],
+          [[  ⠀⠀⠀⠀⢀⠞⠉⠀⠀⡇⠀⠀⠀⠀⡇⠀⠀⢀⣾⠋⡼⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡟⠀⠀⠀⠀⠀⠀⠙⢦⡀⢳⡀⠀⠀⠀⠀⠀⢸⠀⠀⠀]],
+          [[  ⠀⠀⠀⡶⠃⠀⢀⠄⣰⡇⠀⠀⠀⢠⠇⠀⢀⡾⠁⠀⡇⠀⡏⠀⠀⠀⠀⠀⣠⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⣇⠀⠀⠀⢦⠀⠀⠀⠀⠙⠦⣷⠀⠀⠀⠀⠀⢸⠀⠀⠀]],
+          [[  ⠀⠀⢠⠇⠀⣰⣣⠔⢹⠀⠀⠀⠀⢸⠀⠀⡜⠀⠀⠀⡇⢠⡇⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠹⡄⠀⠂⢸⠀⠀⠀⠀⠀⠀⠈⠁⠀⠀⠀⠀⢸⠀⠀⠀]],
+          [[  ⠀⠀⣸⠀⢠⢻⠃⠀⣸⠀⠀⠀⠀⢸⠀⡼⢥⡀⠀⠀⢣⢸⢻⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⢀⡿⡀⠀⠀⠀⠀⠀⢠⣇⢳⠀⠀⠀⡇⢠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀]],
+          [[  ⠀⠀⡇⠀⡎⡏⠀⠀⣿⠀⠀⠀⠀⢸⢰⠁⠀⠙⢶⣄⣸⠸⢻⡀⠀⠀⠀⠀⡇⠀⡄⠀⠀⠀⠀⢸⢰⡇⠀⠀⠀⠀⠀⢸⠈⢿⡆⠀⠀⢻⠘⠀⠀⠀⠀⠀⠀⣰⠀⠀⠀⢸⠀⠀⠀]],
+          [[  ⠀⠀⡇⢰⢣⠁⠀⢸⠻⠀⠀⠀⠀⣿⠆⠀⠀⠀⠀⠉⢻⣿⣬⣇⠀⠀⠀⠀⣇⠀⡇⠀⠀⠀⠀⣾⠏⡇⢤⠀⠀⠀⠀⣇⡠⠬⢷⠀⠀⢸⡆⠀⠀⠀⠀⠀⠀⠇⠀⠀⠀⢸⠀⠀⠀]],
+          [[  ⠀⠀⢳⢸⢸⠀⠀⢸⡆⠀⠀⠀⠀⡿⢖⣒⣶⣤⣄⡓⠾⣯⣙⠺⣀⠀⠀⠀⣿⠀⣿⠀⠀⠀⢠⠏⢀⡁⣼⠀⠀⢠⣿⠁⠀⠀⠈⡆⠀⠀⡇⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⡿⠀⠀⠀]],
+          [[  ⠀⠀⢸⣸⣸⠀⠀⠀⡇⠀⠀⠀⠀⡇⠀⠙⠛⠛⠻⠿⣿⣿⣝⡳⣿⣧⠀⠀⡿⡄⡧⡆⠀⠀⣸⣏⣛⡇⣿⠀⢀⣾⡏⠀⠀⠀⠀⢱⠀⢠⠃⠀⠀⠀⠀⠀⡏⠑⣆⠀⠀⡇⠀⠀⠀]],
+          [[  ⠀⠀⠀⡇⢻⠀⠀⠀⡇⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⠚⠏⢧⠀⡇⢇⢱⢱⡀⠀⣇⠤⠒⣿⢻⢀⣎⣈⣠⣤⣤⣤⣄⣈⣇⢸⠀⠀⠀⠀⠀⣸⠋⠳⣼⠀⢐⠇⠀⠀⠀]],
+          [[  ⠳⣄⠀⢱⠈⠀⠀⠀⡇⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠁⠁⠈⠙⠠⢥⣈⠀⠐⠚⠛⡿⠛⠛⠛⠛⠻⠛⠻⠗⠚⠻⡟⠀⠀⠀⠀⢀⡏⠉⢚⡏⠀⢸⠀⠀⠀⢠ ]],
+          [[  ⣷⣈⠳⣌⡇⠀⠀⠀⢹⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⣸⠀⠀⡜⠀⠀⢸⠀⢀⡴⣿]],
+          [[  ⣿⣿⣷⡈⢻⠀⠀⠀⢸⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡇⠈⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠁⠀⠀⠀⢀⠇⢀⡼⠁⠀⠀⣤⢔⣿⣿⣿]],
+          [[  ⣿⣿⣿⣷⣶⣆⠀⠀⢸⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢚⠓⠲⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⢸⣠⠞⠀⠀⠀⠀⣿⣿⣿⠟⠁]],
+          [[  ⣿⣿⣿⣿⣿⣿⡄⠀⠘⡆⠀⠀⠀⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡏⠀⠀⠀⠀⡞⠁⠀⠀⠀⠀⠀⣿⠟⠉⠀⠀]],
+          [[  ⣿⣿⣿⣿⣿⣿⣷⡀⠀⡇⠀⠀⠀⢹⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠇⠀⠀⠀⢠⠇⠀⠀⠀⠀⠀⢠⡇⠀⠀⠀⠀]],
+          [[  ⣿⣿⣿⣿⣿⣿⣿⣧⠀⢧⠀⠀⠀⢸⢳⡀⠀⠀⠀⠀⠀⠀⢀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣴⡀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠀⠀⠀⠀⡸⠀⠀⠀⠀⠀⠀⡸⡇⠀⠀⠀⠀]],
+          [[  ⣿⣿⣿⣿⣿⣿⣿⣿⡆⢸⠀⠀⠀⠸⡆⠑⣄⠀⠀⠀⠀⠀⠈⣿⢍⠉⠑⠒⠒⠒⠒⠒⠈⠉⢉⣁⣀⣾⡇⠀⠀⠀⠀⠀⠀⢀⠞⡏⠀⠀⠀⢀⡇⠀⠀⠀⠀⠀⢀⡇⡇⠀⠀⠀⠀]],
+          [[  ⣿⣿⣿⣿⣿⣿⣿⣿⣿⡜⡆⠀⠀⠀⡇⠀⡇⠑⢄⡀⠀⠀⠀⠘⠲⠤⣀⣀⡀⠀⠀⠀⠀⣁⣉⡤⠼⠛⠁⠀⠀⠀⠀⢀⠔⠁⠀⡇⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⡼⠀⡇⠀⠀⠀⠀]],
+          [[  ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠁⠀⠀⠀⡇⠀⡇⠀⠀⠙⢦⡀⠀⠀⠀⠀⠀⠀⠉⠁⠉⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⢀⡴⠋⠀⠀⢀⠀⠀⠀⠀⡎⠀⠀⠀⠀⠀⢠⠇⠀⣧⡀⠀⠀⠀ ]],
+          [[  ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⠀⠀⠀⢀⠀⣇⠀⠀⠀⠀⠉⠢⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡴⠚⠁⠀⠀⠀⠀⢸⠀⠀⠀⢠⠃⠀⠀⠀⠀⠀⡜⠀⢀⡲⠙⠢⡀⠀ ]],
+          [[  ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⠀⠀⢸⠀⡇⠳⡄⠀⠀⠀⠀⠈⠢⡀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡤⠔⡻⠋⠀⠀⠀⠀⠀⠀⠀⡞⠀⠀⠀⢸⠀⠀⠀⠀⠀⢀⠃⠀⢸⡶⠀⠁⠹⢦   ]],
+          [[  ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠸⡄⣧⠀⠈⠢⡀⠀⠀⠀⠀⠈⠲⢄⣀⣤⣤⠒⠊⠉⢀⡠⠞⠁⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⠀⠀⢸⠀⣾⣦⣆⡈   ]],
+          [[  ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⡇⣟⣣⠀⠀⠙⢆⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠓⠒⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⠃⠀⠀⠀⡇⠀⠀⠀⠀⢠⠇⠀⠀⣼⣸⣿⣿⣿⣿   ]],
+          [[  ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⠀⠀⢹⣿⣿⣧⡀⠀⠀⠑⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⠀⠀⠀⢀⠇⠀⠀⠀⠀⡜⠀⠀⠀⡇⣿⣿⣿⣿⣿   ]],
+        },
+          -- Waifu 3
+        {
+          [[  ⠀⠀⠀⠀⠀⠀⠀⢀⣤⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
+          [[  ⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
+          [[  ⠀⠀⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
+          [[  ⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀]],
+          [[  ⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀]],
+          [[  ⠀⣰⣿⣿⣿⣿⣿⣿⢻⣿⣿⠏⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀]],
+          [[  ⢀⣿⣿⣿⣿⣿⡟⣿⡟⠘⣿⡇⢞⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀]],
+          [[  ⢸⣿⣿⣿⣿⣿⣷⣿⡇⠀⠹⣿⣧⠼⢾⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀]],
+          [[  ⣿⣿⣿⣿⣿⣿⣿⣹⣗⠒⠋⠙⢿⣆⠈⢫⣿⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⠀⠀⠀⠀]],
+          [[  ⣿⣿⣿⣿⣿⣻⣿⠘⢿⠀⠠⣖⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣿⠚⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀]],
+          [[  ⢹⣿⣿⣿⣿⣿⣿⡄⠀⠣⠀⠻⠋⠀⠘⣿⠿⠛⢆⢻⣿⣿⣿⣿⣿⢈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀]],
+          [[  ⣼⣿⣿⣿⣿⠛⣿⡷⣀⠀⠀⠀⠀⠉⠉⠀⠀⠀⠘⣄⣿⣿⣿⣿⣿⣍⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⡀⠀⠀]],
+          [[  ⡇⣿⣿⣿⣿⡗⠈⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⡇⠀⠀]],
+          [[  ⡇⢿⣿⣿⣿⣷⠾⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⡇⠀⠀]],
+          [[  ⡇⢸⣿⣿⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣹⡿⢿⠿⠒⠓⠒⠂]],
+          [[  ⢣⠸⣿⣿⣿⣿⣷⡀⠀⠀⣀⣤⡀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢇⡿⠋⠀⠁⠀⠀⠀⠀⠀]],
+          [[  ⠸⠀⣿⣿⣿⣿⣿⣷⣄⡀⠘⠭⠄⠀⠀⠀⠀⠀⠀⡗⣿⣿⣿⢸⣿⣿⣿⣿⡟⣽⢿⠞⢀⣀⣀⣀⠀⠀⠀⠀⠀⠀]],
+          [[  ⠀⢇⢹⣿⢹⣿⣿⣿⣿⠙⣦⡀⠀⠀⠀⠀⣀⡤⠴⢿⣿⣿⣿⡏⠀⣿⡟⣿⡿⠋⢰⠋⡸⠉⠁⠀⠀⢀⡤⠚⠁⠀⠀]],
+          [[  ⠀⠈⠛⣿⢀⢿⣿⡿⣿⠀⣿⡙⠲⠒⠒⠉⠙⠢⠀⣿⣿⣿⣿⡇⠀⣹⠙⠏⠁⢀⠎⢠⠃⠀⠀⡠⠖⠁⠀⠀⠀⠀⠀]],
+          [[  ⠀⠀⠀⠹⣿⠀⢻⣷⡿⡐⠋⠁⠀⠀⠀⠀⠀⠀⣰⢟⣿⢏⡞⢻⢷⠏⠀⠀⠀⢸⣰⠃⢀⡤⠊⠀⡠⠴⠒⠉⠀⠀⠀]],
+          [[  ⠀⠀⠀⠀⠹⣇⠀⠙⠢⠀⠀⠀⠀⠀⠀⠀⠀⠚⠁⠞⢁⣾⠃⡇⢸⣆⠀⠀⠀⢀⣇⡰⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
+          [[  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⢻⠀⠀⢸⡿⣧⡀⠀⡼⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],  
+        },
+          -- Waifu 4
+        {
+          [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⢤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀       ]],
+          [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⠸⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀       ]],
+          [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡏⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀       ]],
+          [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣷⡀⠘⣇⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀       ]],
+          [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠘⡄⠀⠀⠀⠀⠀⠀⣀⣴⣶⣿⣿⣿⣿⡆⠀⠙⢿⣿⣿⣿⣶⣦⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀       ]],
+          [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⢳⡀⠀⠀⣠⣴⣿⣿⣿⣿⣿⣿⣿⡟⠇⠀⢠⣦⠹⢿⣿⣿⣿⣿⣿⣿⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀       ]],
+          [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⢸⣇⠀⠙⢀⣾⡿⠿⣛⣛⣭⣭⣥⣤⣤⡍⢄⠀⢼⠅⣾⣦⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀       ]],
+          [[ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⠄⠰⢛⣥⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣎⣋⠴⣶⣦⣍⡻⣿⣿⣿⣿⣿⣿⣿⣿⣯⡢⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀       ]],
+          [[ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⢻⣿⡿⣿⣿⡏⢻⡆⣌⢻⣯⡁⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀       ]],
+          [[ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣷⣾⣿⣿⣿⣿⣿⣿⣿⡇⢸⣿⡇⣿⣿⡇⢸⡇⢿⣆⠻⡇⣼⣿⣿⣿⣿⣿⣧⢿⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀       ]],
+          [[ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿⣸⣿⢸⣿⣿⣿⡇⡆⣿⡇⢻⣿⡇⠀⡇⣈⠻⣧⡙⢿⣿⣿⢹⣿⣿⣿⠸⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀       ]],
+          [[ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⢿⡇⣿⣿⢸⣿⣿⣿⡇⡇⢿⡇⢸⣿⣷⠀⠇⣿⣶⡙⣷⣾⣿⣿⢸⣿⡟⢿⡄⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀       ]],
+          [[ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⢻⣿⣿⢸⡇⣿⣿⠘⡟⣿⣿⡇⡇⢸⡇⠀⣋⣥⢀⡆⣿⣿⣦⡈⢿⣿⣿⣼⣿⠧⠠⠡⢻⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀       ]],
+          [[ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⡇⣿⣿⣿⣿⡇⢿⣿⠃⡇⣿⣿⣷⠀⣅⣆⠘⠉⠁⠀⢁⣈⢛⠛⠃⢸⣿⣿⣿⣿⡆⢃⠀⢸⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀       ]],
+          [[ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠁⣿⢸⣿⢿⡇⢸⣛⠀⠹⠘⡇⣿⣸⣼⢏⣠⣆⠸⠅⠀⢙⣀⣾⡇⢸⣿⣿⢹⣿⠃⠀⠀⠈⢸⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀       ]],
+          [[ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⣿⠸⣿⠈⣧⢀⠙⠁⠀⠆⣿⣼⣿⣿⣿⣿⣶⣶⣶⣾⣿⣿⣿⡇⢸⢹⡇⣿⣿⠀⢶⣭⠃⢸⣿⢻⣷⠀⠀⠀⠀⠀⠀⠀       ]],
+          [[ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⡄⢉⠀⢿⡔⠹⠀⣶⣦⠇⠂⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠈⣼⣿⣿⡟⣰⣦⣻⡷⢸⣿⣾⡏⠀⠀⠀⠀⠀⠀⠀       ]],
+          [[ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡖⠂⠀⠸⡌⣧⣷⠁⢹⣧⣖⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⢠⣿⣿⣿⡇⢿⡝⣿⠇⣌⣿⡟⣿⡆⠐⠀⠀⠀⠀⠀       ]],
+          [[ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⠏⠀⡐⣰⣷⢠⢹⣿⡇⢸⣿⣿⣷⣌⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⣼⣿⣿⣿⢁⣨⠄⠋⠀⣿⣿⡇⠿⢿⡀⠂⠀⠀⠀⠀       ]],
+          [[ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⠇⢀⡜⣱⣿⡿⠈⢸⣿⣇⠀⣿⣿⣿⣿⣿⠿⠿⠿⠿⣿⣿⣿⣿⣿⣿⡿⢠⣿⢏⣿⡏⣘⠋⠀⠄⣇⢳⣿⣿⠀⠂⢻⡀⠀⠀⠀⠀       ]],
+          [[ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⠏⣠⡿⢃⣾⣿⠇⠼⢸⣿⣿⢰⠘⢿⣿⣿⣿⣦⣿⣿⣷⣾⣿⣿⣿⣿⣿⠃⢸⣿⢸⣿⣿⣿⠀⠀⠀⢸⢸⣿⣿⡄⡀⠀⢿⡀⠀⠀⠀       ]],
+          [[ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣷⣾⠟⢁⣿⣿⡟⡘⣼⢸⣿⣿⢸⠈⠀⡙⢿⣿⣿⣧⣤⣿⣿⣿⣿⣿⣿⠇⡆⣸⡇⣿⣿⣿⣿⠀⣦⢠⠈⠘⣿⣿⡇⠻⠀⠀⢻⡀⠀⠀       ]],
+          [[ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⡿⠃⢠⣾⣿⡟⠀⣸⣿⢸⣿⣿⢸⠀⢸⣿⡦⡙⢿⣿⣿⣿⣿⣿⣿⠿⢋⣠⡆⡿⢸⣿⣿⣿⣿⠀⠛⠈⣿⡆⣿⣿⣿⡸⣦⠀⠀⢻⡄⠀       ]],
+          [[ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣟⠟⠀⢀⣾⣿⡟⠀⣴⣿⠁⣿⣿⣿⢸⢀⣿⣿⢡⣿⣦⠙⠿⠟⠛⠉⣤⣴⢁⣿⢠⢇⣿⡟⢸⣭⣴⣶⢀⢣⣿⡇⢿⣿⣿⡇⢿⡆⠰⠀⢻⣄       ]],
+          [[ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣶⣿⣿⡇⠀⢠⣿⣿⣿⢃⣼⣿⡏⣎⣿⣿⣿⢸⢁⣿⠏⣼⣿⣿⡀⢸⣿⡇⠀⢿⡏⡾⠟⡈⣼⣿⢱⣿⣿⣿⣿⠈⢸⣿⢷⢸⣿⣿⣿⠸⣷⡀⠂⠀⢿        ]],
+          [[ ⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⡿⠋⣽⠄⣰⣿⣿⡟⠑⠿⣿⡿⠀⣾⡟⢹⣿⢸⢸⡟⢠⣿⣿⣿⠀⢸⣿⠁⠀⠈⢁⣶⣇⣠⣿⡏⣾⣿⣿⣿⣿⡄⠘⣿⠘⠀⢿⣿⣿⡆⣿⣷⡈⠀⠈        ]],
+          [[ ⠀⠀⠀⠀⠀⠀⣠⣾⣿⠟⠀⠀⡟⣸⣿⡿⢫⣾⣿⣷⡿⠁⣽⠟⡡⣸⣿⢸⡞⠀⣈⣿⣿⡇⣸⢸⡿⠀⢸⠀⢸⡿⢸⣿⡿⢸⣿⣿⡿⠟⠛⣃⠀⣿⡇⡆⠸⣿⣿⣿⠘⣿⡧⠐⠀       ]],
+          [[ ⠀⠀⠀⠀⢀⣴⣿⡿⠃⠀⠀⣰⣿⣿⡿⢡⣄⠻⠿⠟⢡⣴⠏⣼⡇⣿⡇⠀⣰⢃⣿⡿⣿⠁⡇⣾⡇⠀⠞⣠⣾⢃⣿⣿⢃⣿⢟⣵⡖⢋⣉⡉⡉⣹⡷⢀⠀⣿⣿⣿⣇⠻⣷⣄⠀       ]],
+          [[ ⠀⠀⠀⢀⣾⣿⠟⠀⠀⢀⣾⣿⠿⠍⣠⣍⢻⠶⢋⣴⠿⢡⣾⣿⠁⣿⡇⢸⡏⣸⣿⢃⡟⣸⢀⡿⢃⢰⣿⣿⢸⣿⣿⠋⢈⣁⣸⣿⣿⣷⣶⡾⢟⡘⢿⣶⣶⣶⣶⣮⣭⣤⡹⣿⣆       ]],
+          [[ ⠀⠀⢠⣿⣿⠏⣠⣽⣰⣿⡋⣤⣶⡌⣿⡟⢠⣾⡇⠁⣠⣿⡿⠁⡀⣿⡇⣿⢡⣿⠏⡾⢀⣹⡸⢁⣠⣿⣿⡏⢈⣿⡟⠀⣛⡻⠁⠙⠌⠛⢯⣴⣿⠟⣈⠻⢦⡶⢶⣶⠖⣢⣍⠸⣿       ]],
+          [[ ⠀⢠⣿⣿⠏⠀⢿⣿⡇⣿⣇⢸⣿⡇⣿⢻⣿⢏⠀⣰⣿⠟⡰⢃⡄⣿⢡⡏⣾⢏⣼⡣⠟⣫⣴⣿⡿⠟⣉⠄⣼⣿⢡⣼⠟⠀⠀⢀⣤⢀⣴⠟⣥⣾⣿⣿⣶⣤⣤⣶⣾⣿⣿⣿⡄       ]],
+          [[ ⢀⣾⣿⠟⣠⣾⡾⢿⡇⠹⣿⢸⡟⣠⣦⣾⢃⢮⣾⡿⢋⠔⣠⣾⡧⣿⢸⠟⢃⡾⠋⢀⠾⣿⣿⢿⣰⡿⢏⣤⣿⠃⠜⡁⠀⠀⢠⠞⣁⡾⢁⣼⣿⣿⣿⣿⣿⠿⠿⣿⣿⣿⡿⣿⣿       ]],
+        },
+          -- Waifu 5
+        {
+          [[ ⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷ ]],
+          [[ ⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇ ]],
+          [[ ⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽ ]],
+          [[ ⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕ ]],
+          [[ ⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕ ]],
+          [[ ⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕ ]],
+          [[ ⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄ ]],
+          [[ ⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕ ]],
+          [[ ⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿ ]],
+          [[ ⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ]],
+          [[ ⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟ ]],
+          [[ ⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠ ]],
+          [[ ⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙ ]],
+          [[ ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣ ]],
+        },
+      }
+
+      -- Semilla aleatoria y selección
+      math.randomseed(os.time())
+      dashboard.section.header.val = headers[math.random(#headers)]
+
+      dashboard.section.buttons.val = {
+        dashboard.button("e", "►  Nuevo archivo", ":ene <BAR> startinsert <CR>"),
+        dashboard.button("f", "■  Buscar archivo", ":Telescope find_files<CR>"),
+        dashboard.button("r", "▲  Archivos recientes", ":Telescope oldfiles<CR>"),
+        dashboard.button("q", "◆  Salir", ":qa<CR>"),
+      }
+
+      dashboard.section.footer.val = 
+        "✦  想像できれば、プログラムできます。✦"
+
+      alpha.setup(dashboard.config)
+    end,
+  },
+
+  -- Barra de estado
+  { "nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" } },
+
+  -- Explorador de archivos
+  { "nvim-tree/nvim-tree.lua", dependencies = { "nvim-tree/nvim-web-devicons" } },
+
+  -- Telescope (buscador)
+  { "nvim-telescope/telescope.nvim", tag = "0.1.5", dependencies = { "nvim-lua/plenary.nvim" } },
+
+  -- Treesitter (resaltado moderno)
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+
+  -- Autocompletado
+  { "hrsh7th/nvim-cmp" },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/cmp-buffer" },
+  { "hrsh7th/cmp-path" },
+  { "hrsh7th/cmp-cmdline" },
+
+  -- Snippets
+  { "hrsh7th/vim-vsnip" },
+  { "hrsh7th/cmp-vsnip" },
+
+  -- LSP (Lenguajes)
+  { "neovim/nvim-lspconfig" },
+  { "williamboman/mason.nvim" },
+  { "williamboman/mason-lspconfig.nvim" },
+})
+
+-- =========================
+-- Opciones generales
+-- =========================
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.termguicolors = true
+vim.opt.cursorline = true
+vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamedplus"
+
+-- Colores
+vim.cmd([[colorscheme tokyonight]])
+
+-- =========================
+-- Configuración de plugins
+-- =========================
+
+-- Lualine
+require("lualine").setup({
+  options = {
+    theme = "tokyonight",
+    section_separators = { "✦", "▸" },
+    component_separators = { "✦", "►" },
+  },
+  sections = {
+    lualine_a = { "mode" }, -- Modo (NORMAL, INSERT, VISUAL...) con color
+    lualine_b = { "branch", "diff", "diagnostics" }, -- Rama Git, cambios y diagnósticos LSP
+    lualine_c = { "filename" }, -- Nombre del archivo activo
+    lualine_x = { "encoding", "fileformat", "filetype" }, -- UTF-8, formato y tipo de archivo
+    lualine_y = { "progress" }, -- Progreso en el archivo
+    lualine_z = { "location" }, -- Línea y columna
+  },
+})
+
+-- Nvim-tree
+require("nvim-tree").setup({
+  view = {
+    width = 35,
+    side = "left",
+    number = true,
+    relativenumber = true,
+  },
+  renderer = {
+    highlight_opened_files = "name",
+    indent_markers = { enable = true },
+    highlight_git = true, -- Colores para estado Git
+    icons = {
+      show = {
+        git = true,
+        folder = true,
+        file = true,
+        folder_arrow = true,
+      },
+    },
+  },
+  git = {
+    enable = true, -- Asegura que Nvim-tree lea el estado Git
+  },
+})
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle Explorer" })
+
+-- Telescope
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Buscar archivos" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Buscar texto" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buscar buffers" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Buscar ayuda" })
+
+-- Treesitter
+require("nvim-treesitter.configs").setup({
+  ensure_installed = {
+    -- Core
+    "lua",
+    "vim",
+    "vimdoc",
+
+    -- Frontend / Angular
+    "javascript",
+    "typescript",
+    "html",
+    "css",
+    "scss",
+    "json",
+
+    -- Backend / Fullstack
+    "python",
+    "php",
+    "sql",
+
+    -- Utilidades
+    "bash",
+    "markdown",
+    "markdown_inline",
+    "yaml",
+    "dockerfile",
+    "gitignore"
+  },
+  highlight = { enable = true },
+  indent = { enable = true }
+})
+
+-- Mason (gestor de LSP/DAP/formatters)
+require("mason").setup()
+require("mason-lspconfig").setup({
+  ensure_installed = { "lua_ls", "pyright", "ts_ls" }
+})
+
+-- Configuración LSP
+local lspconfig = require("lspconfig")
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+lspconfig.lua_ls.setup({ capabilities = capabilities })
+lspconfig.pyright.setup({ capabilities = capabilities })
+lspconfig.ts_ls.setup({ capabilities = capabilities })
+
+-- Autocompletado con snippets
+local cmp = require("cmp")
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+  }),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "vsnip" },
+    { name = "buffer" },
+    { name = "path" },
+  })
+})
+
+
+-- =========================
+-- Atajos rápidos (Keymaps)
+-- =========================
+
+-- Guardar y salir
+vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Guardar" })
+vim.keymap.set("n", "<leader>q", ":q<CR>", { desc = "Cerrar" })
+vim.keymap.set("n", "<leader>Q", ":q!<CR>", { desc = "Forzar cerrar sin guardar" })
+
+-- Quitar highlight
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>", { desc = "Quitar highlight" })
+
+-- Manejo de ventanas
+vim.keymap.set("n", "<leader>sv", ":vsplit<CR>", { desc = "Dividir vertical" })
+vim.keymap.set("n", "<leader>sh", ":split<CR>", { desc = "Dividir horizontal" })
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Ir izquierda" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Ir derecha" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Ir abajo" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Ir arriba" })
+
+-- Manejo de buffers
+vim.keymap.set("n", "<S-l>", ":bnext<CR>", { desc = "Buffer siguiente" })
+vim.keymap.set("n", "<S-h>", ":bprevious<CR>", { desc = "Buffer anterior" })
+vim.keymap.set("n", "<leader>c", ":bd<CR>", { desc = "Cerrar buffer" })
+
+-- Explorador de archivos
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Abrir explorador" })
+
+-- Telescope
+vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Buscar archivos" })
+vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", { desc = "Buscar texto" })
+vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>", { desc = "Buscar buffers" })
+vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", { desc = "Buscar ayuda" })
+
+-- LSP / Diagnósticos
+vim.keymap.set("n", "<leader>f", function()
+  vim.lsp.buf.format({ async = true })
+end, { desc = "Formatear código" })
+
+vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Ver error" })
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Error anterior" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Error siguiente" })
+
+-- dashboard
+vim.keymap.set("n", "<leader>a", ":Alpha<CR>", { desc = "Volver al Dashboard" })
---
+++ readme.md
@@ -0,0 +1,94 @@
+# 🌸 Neovim Config (Windows)
+
+Configuración personalizada de **Neovim** para Windows, con soporte para LSP, autocompletado, dashboard con waifus y tema **Tokyonight**.  
+
+![preview](./screenshot.png) <!-- (opcional: captura de tu dashboard) -->
+
+---
+
+## 📦 Requisitos
+
+Antes de empezar, instala:
+
+1. **Neovim**  
+   Descargar desde [neovim.io](https://neovim.io/) o con [Scoop](https://scoop.sh/):
+   ```powershell
+   scoop install neovim
+   
+   ```
+2. **Administrador de paquetes Scoop**
+    ```powershell
+    Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+    irm get.scoop.sh | iex
+
+    ```
+3. **Fuente Nerd Font (para iconos en la barra y dashboard).
+    - Use [JetBrais Mono Nerd Font](https://www.nerdfonts.com/).
+        - Descargar el .zip
+        - Extraelos
+        - Selecciona todos los .ttf -> Instalalos todos
+
+4. **MSYS2 para toolchain de C (necesario para Treesitter y LSP)**
+    - Descargar desde: [MSYS2](https://www.msys2.org/).
+    - Luego abrir MSYS2 MinGW UCRT64 y ejacutar el siguiente comando
+    ```powershell
+    pacman -Syu
+    pacman -S --needed base-devel mingw-w64-x86_64-toolchain
+
+    ```
+    - Esto instala `gcc`, `make`, etc.
+
+---
+# ⚙️ Instalación
+
+1. Clona este repositorio en tu carpeta de Configuración de Neovim
+```powershell
+git clone https://github.com/AngelArgumedo/nvim-config.git $env:LOCALAPPDATA\nvim
+
+```
+
+2. Abre Neovim
+```powershell
+nvim
+
+```
+
+3. La primera vez, lazy.nvim instalara automaticamente los plugins.
+espera a que termine y reinicia neovim
+
+---
+# 🎨 Plugins incluidos
+- Tema: tokyonight.nvim
+
+- Dashboard con waifu ASCII: alpha-nvim, enlace para arte [ASCII](https://emojicombos.com/waifu-ascii-art).
+
+- Barra de estado: lualine.nvim
+
+- Explorador de archivos: nvim-tree.lua
+
+- Buscador: telescope.nvim
+
+- Resaltado avanzado: nvim-treesitter
+
+- Autocompletado: nvim-cmp + fuentes
+
+- Snippets: vim-vsnip
+
+- LSP: nvim-lspconfig, mason.nvimema: tokyonight.nvim
+
+- Dashboard con waifu ASCII: alpha-nvim
+
+- Barra de estado: lualine.nvim
+
+- Explorador de archivos: nvim-tree.lua
+
+- Buscador: telescope.nvim
+
+- Resaltado avanzado: nvim-treesitter
+
+- Autocompletado: nvim-cmp + fuentes
+
+- Snippets: vim-vsnip
+
+- LSP: nvim-lspconfig, mason.nvim
+
